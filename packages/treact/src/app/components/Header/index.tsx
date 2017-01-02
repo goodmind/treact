bf255d2fbb72d727891e291a05beeee816cb590b
goodmind <andwebar@gmail.com>
2017-01-02 17:26:16 +0500

    Merge branch 'zerobias-master'

--- packages/treact/src/app/components/Header/index.tsx
+++ packages/treact/src/app/components/Header/index.tsx
@@ -1,13 +1,14 @@
 import * as React from 'react';
 import { Link } from 'react-router';
 
+const logo = require('./title-logo.png');
+const s = require('./style.css');
+
 class Header extends React.Component<any, any> {
   public render() {
-    const s = require('./style.css');
-
     return (
       <header className={s.header}>
-        <img className={s.headerIcon} src={require('./title-logo.png')} />
+        <img className={s.headerIcon} src={logo} />
         <Link to="/">Home</Link>
         <Link to="login">Login</Link>
       </header>
