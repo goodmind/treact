565fb8ccccae62409f8476d678c01d032b52a7cb
goodmind <andwebar@gmail.com>
2017-01-06 21:29:09 +0500

    refactor(Login/Intro): add props types, pure component

--- packages/treact/src/app/containers/Login/steps/Intro/index.tsx
+++ packages/treact/src/app/containers/Login/steps/Intro/index.tsx
@@ -1,25 +1,27 @@
 import * as React from 'react';
 const t = require('../../style.css');
 
-class Intro extends React.Component<any, any> {
-  public render() {
-    return (
-      <div className={t.loginStep}>
-        <img
-          width={102}
-          height={102}
-          src={require('./icon102.png')} />
-        <h1>Telegram Desktop</h1>
-        <p>
-          Welcome to the official <a href="https://telegram.org">Telegram</a> desktop app.
-        </p>
-        <p>
-          It's <strong>fast</strong> and <strong>secure</strong>.
-        </p>
-        <button onClick={this.props.nextStep} className={`${t.btn} ${t.primary}`}>Start messaging</button>
-      </div>
-    );
-  }
+interface IProps {
+  nextStep: any;
 }
+
+const Intro = (props: IProps) => {
+  return (
+    <div className={t.loginStep}>
+      <img
+        width={102}
+        height={102}
+        src={require('./icon102.png')} />
+      <h1>Telegram Desktop</h1>
+      <p>
+        Welcome to the official <a href="https://telegram.org">Telegram</a> desktop app.
+      </p>
+      <p>
+        It's <strong>fast</strong> and <strong>secure</strong>.
+      </p>
+      <button onClick={props.nextStep} className={`${t.btn} ${t.primary}`}>Start messaging</button>
+    </div>
+  );
+};
 
 export { Intro }
