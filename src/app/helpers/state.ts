052c3f502fcd0cdad41b97dfda54d7c51e20b874
Dmitry Boldyrev <ribkatt@gmail.com>
2017-01-14 07:04:17 +0300

    Add state helpers

--- src/app/helpers/state.ts
+++ src/app/helpers/state.ts
@@ -1,5 +1,13 @@
 import { TById } from 'redux/mtproto';
-import { pipe as P, map, contains, append, path, fromPairs, dissoc, unless, reduce } from 'ramda';
+import { IReducer } from 'redux/IStore';
+import { rejectDashAndFuncs } from 'helpers/treeProcess';
+import { pipe as P, map, contains, append, path, dissoc, assoc,
+  unless, reduce, prop, propEq, curry } from 'ramda';
+
+const convolveReducers = payload => (state, reducer) => reducer(state, payload);
+
+export const reduceSequence = <S, P>(...reducers): IReducer<S, P> =>
+  (state, payload) => reducers.reduce(convolveReducers(payload), state);
 
 export const whenNot = (pred, change) =>
   (state, data) =>
@@ -8,6 +16,12 @@ export const whenNot = (pred, change) =>
       change(data),
     )(state);
 
+export const whenNotC = curry( (pred, change, data, state) =>
+  unless(
+    pred(data),
+    change(data),
+  )(state) );
+
 export interface IStoreList<T> {
   ids: number[];
   byId: TById<T>;
@@ -15,7 +29,9 @@ export interface IStoreList<T> {
 
 export const isEmptyList = <T>(storeField: IStoreList<T>) => storeField.ids.length === 0;
 
-export const getReduce = (getter, reducer) => (state, payload) =>
+type IGetReduce = <S, PL, G>(getter: (pl: PL) => G, reducer: IReducer<S, G>) => IReducer<S, PL>;
+
+export const getReduce: IGetReduce = (getter, reducer) => (state, payload) =>
   P(getter, reduce(reducer, state))(payload);
 
 export const getListOf = (field: string) => path([field, 'list']);
@@ -25,6 +41,30 @@ export const appendNew = whenNot(contains, append);
 export const changePayload = (func, reducer) =>
   (state, payload) => reducer(state, func(payload));
 
+const getId = prop('id');
+
+const addNewId = changePayload(getId, appendNew);
+
+export const newIdsFromList = listGetter => getReduce(listGetter, addNewId);
+
 const idPair = e => [ e.id, dissoc('id', e) ];
 
-export const listToIdMap = P(map(idPair), fromPairs);
+const addChangedProp = <V>(state: TById<V>, [id, val]: [number, V]): TById<V> =>
+  unless(
+    propEq(id, val),
+    assoc(id, val),
+  )(state);
+
+type ITransformer = <F, G>(payload: G) => Array<[number, F]>;
+
+export const changeReducer = <G>(transformer: ITransformer) =>
+  <F, PL>(vectorGetter: (payload: PL) => G) =>
+    getReduce<TById<F>, PL, Array<[number, F]>>(
+      P(vectorGetter, transformer),
+      addChangedProp);
+
+const adaptFieldVector = P(
+  rejectDashAndFuncs,
+  map(idPair));
+
+export const fieldListToMap = changeReducer(adaptFieldVector);
