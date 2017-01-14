0ec3a8dd2c30bad58d56c1d9135708e16a0a12f2
Dmitry Boldyrev <ribkatt@gmail.com>
2017-01-14 06:27:49 +0300

    Move Message component to separated folder.
    Switch to pure function component

--- src/app/components/Message/index.tsx
+++ src/app/components/Message/index.tsx
@@ -2,7 +2,7 @@ import * as React from 'react';
 
 import classes from 'helpers/classes';
 
-const style = require('./message.css');
+const style = require('./style.css');
 
 interface IOwnProps {
   id: number;
@@ -28,27 +28,17 @@ const Time = ({ date }: { date: number }) => {
   );
 };
 
-class Message extends React.Component<IOwnProps, any> {
-  public css = () => classes({
-    block: {
-      [style.block]: true,
-    },
-    message: {
-      [style.message]: true,
-    },
-  })
-  public render() {
-    const { date, text } = this.props;
-    const { block, message } = this.css();
-    return (
-      <div className={block}>
-        <Time date={date} />
-        <div className={message}>
-          {text}
-        </div>
+export const Message = ({ date, text }: IOwnProps) => {
+  const { block, message } = classes({
+    block: style.block,
+    message: style.message,
+  });
+  return (
+    <div className={block}>
+      <Time date={date} />
+      <div className={message}>
+        {text}
       </div>
-    );
-  }
-}
-
-export default Message;
+    </div>
+  );
+};
