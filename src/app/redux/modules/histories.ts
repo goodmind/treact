8c8245f2e666dd97c8856d0a7663e975278386a4
goodmind <andwebar@gmail.com>
2017-01-14 20:59:33 +0500

    refactor(histories): add generic types

--- src/app/redux/modules/histories.ts
+++ src/app/redux/modules/histories.ts
@@ -1,7 +1,7 @@
 import { createReducer } from 'redux-act';
 import { combineReducers } from 'redux';
-import { assoc, contains, unless, where, evolve, append, has, pipe, unapply,
-  prop, map, cond, pluck, converge, zip, lensPath, pathEq, assocPath, over, reduce, subtract, sort } from 'ramda';
+import { head, assoc, contains, unless, where, evolve, append, has, pipe, unapply, propEq, filter,
+  prop, map, cond, pair, converge, lensPath, pathEq, assocPath, over, reduce, subtract, sort } from 'ramda';
 import { IStoreList, whenNot, whenNotC, getListOf, appendNew } from 'helpers/state';
 import { TById, IMtpMessage } from '../mtproto';
 import { CHATS } from 'actions';
@@ -29,7 +29,7 @@ const newSliceField = field => assoc(field, newSlice());
 
 const onSliceInit = (state: TById<IStoreHistory>, id: number) => whenNot(has, newSliceField)(state, id);
 
-const appendSorted = id => pipe( append(id), sort(subtract) );
+const appendSorted = id => pipe<number[], number[], number[]>( append(id), sort(subtract) );
 
 const addMessage = (state: IStoreHistory, message: IMtpMessage) =>
   unless(
@@ -52,11 +52,15 @@ type TMessageRecord = {
 const onSliceDone = (state: TById<IStoreHistory>, { id, messages }: TMessageRecord) =>
   assoc(id, addMessages(state[id], messages), state);
 
-const chooseField = unapply(pipe( map( e => [ has(e), prop(e) ] ), cond ));
+const chooseField = unapply(pipe( map( (e: string) => [ has(e), prop(e) ] ), cond ));
 
 const getAnyId = chooseField('user_id', 'chat_id', 'channel_id');
 
-const dialogsIds = pipe( getListOf('dialogs'), pluck('peer'), map(getAnyId) );
+const select = converge(pair, [
+  prop('top_message'),
+  pipe( prop('peer'), getAnyId ) ]);
+
+const dialogsIds = pipe( getListOf('dialogs'), map( select ) );
 
 const dialogsReducer = (state: TById<IStoreHistory>, [ id, message ]: [number, IMtpMessage]) => {
   const msgId = message.id;
@@ -75,9 +79,13 @@ const dialogsReducer = (state: TById<IStoreHistory>, [ id, message ]: [number, I
   return pipe( onNotExists, updateChanged )(state);
 };
 
+const zip =
+  (dialogs: any[], msgs: any[]) =>
+    map(([topMessage, id]) => [id, head(filter(propEq('id', topMessage), msgs))], dialogs);
 const linkDialogsMsg = converge(zip, [dialogsIds, getListOf('messages')]);
 
-const onGetDialog = (state: TById<IStoreHistory>, payload: IMtpGetDialogs) => pipe(
+// TODO: strict types
+const onGetDialog = (state: TById<IStoreHistory>, payload: IMtpGetDialogs) => pipe<any, any, any>(
   linkDialogsMsg,
   reduce(dialogsReducer, state),
 )(payload);
