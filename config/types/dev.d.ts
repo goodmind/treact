eba5738d3873b8060d7d6bdfc57d23d8f423d075
goodmind <andwebar@gmail.com>
2017-01-14 23:59:24 +0500

    chore(types/dev): add file APIs types

--- config/types/dev.d.ts
+++ config/types/dev.d.ts
@@ -170,6 +170,11 @@ interface Window {
   // A hack for the Redux DevTools Chrome extension.
   __REDUX_DEVTOOLS_EXTENSION_COMPOSE__?: <F extends Function>(...f: F[]) => F;
   __INITIAL_STATE__?: any;
+  webkitURL?: any;
+  BlobBuilder?: any;
+  WebKitBlobBuilder?: any;
+  MozBlobBuilder?: any;
+  chrome?: any;
 }
 
 interface ObjectConstructor {
