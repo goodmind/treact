db22b81cced32eaeb7373edfcfb8cfcb1a3a25f4
goodmind <andwebar@gmail.com>
2017-01-19 14:58:19 +0500

    refactor(ChatFooter): move from Chat component

---
+++ src/app/components/ChatFooter/index.tsx
@@ -0,0 +1,15 @@
+import * as React from 'react';
+import * as AutoSizeTextarea from 'react-autosize-textarea';
+const s = require('../Chat/style.css');
+
+export const ChatFooter = ({ value, change, submit }) => (
+  <div className={s.chatfooter}>
+    <AutoSizeTextarea
+      onChange={change}
+      className={s.editText}
+      value={value}
+      rows={1}
+      placeholder="  Write a message" />
+    <div className={s.sendbutton} onClick={submit}>Send</div>
+  </div>
+);
