e949677ef3d34fc74834258d05d116c14d8fc3be
Андрей Филиппов <andwebar@gmail.com>
2017-02-01 20:38:39 +0500

    refactor(helpers/Telegram): restore data center from localStorage

--- src/app/helpers/Telegram/helpers.ts
+++ src/app/helpers/Telegram/helpers.ts
@@ -1,15 +1,25 @@
 import * as MTProto from '@goodmind/telegram-mt-node';
+import { DEFAULT_DC_ID } from './constants';
+import { path } from 'ramda';
 
-export function restoreAuthKey() {
+export const getFromStore = key => JSON.parse(localStorage.getItem(`reduxPersist:${key}`));
+export const byIdGetter = id => path(['byId', id]);
+
+export function restoreAuthKeyFromLocalStorage() {
   if (typeof window === 'undefined') {
     return undefined;
   }
 
-  const storageKey = JSON.parse(localStorage.getItem('reduxPersist:authKey'));
+  const currentDc = getFromStore('currentDc') || DEFAULT_DC_ID;
+  const storageKey = byIdGetter(currentDc)<any>(getFromStore('authKey'));
   if (!storageKey) {
     return undefined;
   }
 
+  return restoreAuthKey(storageKey);
+}
+
+export function restoreAuthKey(storageKey) {
   const [id, value] = restoreAuthKeyWithSaltFromStorableBuffer(
     Buffer.from(storageKey));
 
@@ -26,3 +36,4 @@ export function authKeyWithSaltToStorableBuffer(authKey, serverSalt) {
 export function restoreAuthKeyWithSaltFromStorableBuffer(buffer) {
   return [buffer.slice(0, 8), buffer.slice(8, 264), buffer.slice(264, 272)];
 }
+
