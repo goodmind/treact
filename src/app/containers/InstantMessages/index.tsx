abdd2a1ab270b42dcf5c95e74c8ba9f733a6af5b
goodmind <andwebar@gmail.com>
2017-02-07 21:43:06 +0500

    refactor(InstantMessages): fix spread

--- src/app/containers/InstantMessages/index.tsx
+++ src/app/containers/InstantMessages/index.tsx
@@ -4,14 +4,14 @@ import { ChatList } from 'containers';
 import { fetchChatList } from 'api/chatList';
 import { asyncConnect } from 'redux-connect';
 // import { Updates } from 'helpers/Telegram/Updates';
-import DownloadAssistant from './downloadAssistant'
+import DownloadAssistant from './downloadAssistant';
 const s = require('./style.css');
 
 class InstantMessagesImpl extends React.Component<{}, {}> {
   // public updates = Updates.getInstance();
 
-  constructor(...args) {
-    super(...args);
+  constructor(props, context) {
+    super(props, context);
     // this.updates.start(update => {
     //   console.log(update._typeName, update);
     // });
