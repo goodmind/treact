ce03297a62dfd5accce8e757c2118706b6fbec2d
goodmind <andwebar@gmail.com>
2017-02-07 21:42:45 +0500

    refactor(DownloadAssistant): semicolons and restrict types

--- src/app/containers/InstantMessages/downloadAssistant.tsx
+++ src/app/containers/InstantMessages/downloadAssistant.tsx
@@ -1,20 +1,21 @@
 import * as React from 'react';
 import { connect } from 'react-redux';
+import { Action } from 'redux-act';
 import { isNil, isEmpty, unless, when, any } from 'ramda';
 
 import { IStorePhotoCache } from 'redux/modules/photoCache';
 import { IStore, IDispatch } from 'redux/IStore';
 import { CACHE } from 'redux/actions';
-import { IMtpFileLocation } from 'redux/mtproto';
-import pool, { api } from 'helpers/pool';
-import picStore from 'helpers/FileManager/picStore'
+import { IMtpFileLocation, IMtpUploadFile } from 'redux/mtproto';
+import pool, { api } from 'helpers/Telegram/pool';
+import picStore from 'helpers/FileManager/picStore';
 
 import * as localForage from 'localforage';
 // import * as Knack from 'knack';
 // const knack = Knack({ concurrency: 6, interval: 100 })
 // const invoke = knack(api, { onTimeout: Knack.timeouts.reject })
 
-const { LOAD, DONE } = CACHE
+const { LOAD, DONE } = CACHE;
 
 const picStorage = localForage.createInstance({
   driver: localForage.INDEXEDDB, // Force WebSQL; same as using setDriver()
@@ -25,12 +26,12 @@ const picStorage = localForage.createInstance({
 });
 
 interface IPropsStore {
-  photoCache: IStorePhotoCache
+  photoCache: IStorePhotoCache;
 }
 
 interface IPropsDispatch {
-  load: (list: string[]) => any,
-  done: (id: string) => any,
+  load(list: string[]): Action<{}, {}>;
+  done(id: string): Action<{}, {}>;
 }
 
 const beginLoad = async (id: string, loc: IMtpFileLocation) => {
@@ -38,52 +39,52 @@ const beginLoad = async (id: string, loc: IMtpFileLocation) => {
   const inputLocation = Object.assign(
     new pool.client.schema.type.InputFileLocation(),
     { dc_id, volume_id, secret, local_id },
-  )
-  console.warn(`idle`, loc)
+  );
+  console.warn(`idle`, loc);
   const cached = picStorage.getItem<Blob>(id.toString())
     .then(when(isNil, Promise.reject))
     .then(blob => picStore.addBlob(id, blob), () => ({}));
-  const loader = api('upload.getFile', {
+  const loader = () => api<IMtpUploadFile>('upload.getFile', {
     location: inputLocation,
     offset: 0,
     limit: 1024 * 1024,
     dcID: loc.dc_id,
     fileDownload: true,
-    createNetworker: false,
+    createNetworker: true,
     noErrorBox: true,
-  }).then((data: any) => picStorage.setItem(id.toString(), picStore.addPic(id, data.bytes)), () => ({}));
+  }).then(data => picStorage.setItem(id.toString(), picStore.addPic(id, data.bytes)), () => ({}));
   return unless(isNil, loader, await cached);
-}
+};
 // tslint:disable:no-debugger
 const DownloadAssistant = ({ photoCache, load, done }: IPropsStore & IPropsDispatch) => {
-  const { idle, downloaded } = photoCache.cache
-  console.count('DownloadAssistant')
-  console.log(idle.length)
+  const { idle, downloaded } = photoCache.cache;
+  console.count('DownloadAssistant');
+  console.log(idle.length);
   if (any(e => !picStore.has(e), downloaded)) {
-    console.warn('no in picStore!')
+    console.warn('no in picStore!');
   }
   const mapLoad = (id: string) => {
-    let ab = 0
-    ab++
-    const photo = photoCache.photos.byId[id]
+    let ab = 0;
+    ab++;
+    const photo = photoCache.photos.byId[id];
     if (!photo) {
-      return
+      return;
     }
     return beginLoad(id, photo.photo_small)
-    .then(() => done(id), () => ({}))
-  }
-  idle.map(mapLoad)
-  unless(isEmpty, load)(idle)
+    .then(() => done(id), () => ({}));
+  };
+  idle.map(mapLoad);
+  unless(isEmpty, load)(idle);
 
-  return <span />
-}
+  return <span />;
+};
 
-const stateProps = ({ photoCache }: IStore) => ({ photoCache })
+const stateProps = ({ photoCache }: IStore) => ({ photoCache });
 const dispatchProps = (dispatch: IDispatch) => ({
   load: (list: string[]) => dispatch(LOAD(list)),
   done: (id: string) => dispatch(DONE(id)),
-})
+});
 
-const connected = connect(stateProps, dispatchProps)(DownloadAssistant)
+const connected = connect(stateProps, dispatchProps)(DownloadAssistant);
 
-export default connected
+export default connected;
