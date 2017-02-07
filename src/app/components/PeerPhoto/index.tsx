c10b164a9cb5a0d33377375d8c73c4c18e583e85
goodmind <andwebar@gmail.com>
2017-02-07 21:27:21 +0500

    refactor(PeerPhoto): restrict types

--- src/app/components/PeerPhoto/index.tsx
+++ src/app/components/PeerPhoto/index.tsx
@@ -1,16 +1,21 @@
 import * as React from 'react';
 
-import picStore from 'helpers/FileManager/picStore'
+import picStore from 'helpers/FileManager/picStore';
 
-const defPhoto = require('./usercolor1.png')
+const defPhoto = require('./usercolor1.png');
 
-export const PeerPhotoEmpty = ({ className }) =>
+interface IProps {
+  id?: number;
+  className?: string;
+}
+
+export const PeerPhotoEmpty = ({ className }: IProps) =>
   <img
     className={className}
     src={defPhoto} />;
 
-export const PeerPhoto = ({ id, className }) =>
+export const PeerPhoto = ({ id, className }: IProps) =>
   <img
     className={className}
-    src={picStore.get(id)} />
+    src={picStore.get(id)} />;
 // NOTE Sending blob through props is definitely slower
