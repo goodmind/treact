2bf3ca4abc7dc834a642ac447a996c255147e1be
goodmind <andwebar@gmail.com>
2017-02-07 21:24:03 +0500

    style(AppProvider): semicolons

--- src/AppProvider.tsx
+++ src/AppProvider.tsx
@@ -1,26 +1,26 @@
-import * as React from 'react'
-import { Provider } from 'react-redux'
-import { persistStore } from 'redux-persist'
-import { browserHistory } from 'react-router'
+import * as React from 'react';
+import { Provider } from 'react-redux';
+import { persistStore } from 'redux-persist';
+import { browserHistory } from 'react-router';
 
-import pool from 'helpers/pool'
-import { configureStore } from './app/redux/store'
+import pool from 'helpers/Telegram/pool';
+import { configureStore } from './app/redux/store';
 
 export const store = configureStore(
   browserHistory,
   window.__INITIAL_STATE__,
-)
+);
 
 
 
 class AppProvider extends React.Component<{ children?: React.ReactChildren }, { rehydrated: boolean }> {
-  public state = { rehydrated: false }
+  public state = { rehydrated: false };
 
   public componentWillMount() {
-    const onDone = () => this.setState({ rehydrated: true })
+    const onDone = () => this.setState({ rehydrated: true });
     const onConnect = () => persistStore(store,
-      {whitelist: ['authKey', 'currentUser']}, onDone)
-    pool.dcConnect().then(onConnect)
+      {whitelist: ['authKey', 'currentUser', 'currentDc']}, onDone);
+    pool.dcConnect().then(onConnect);
   }
 
   public render() {
@@ -28,8 +28,8 @@ class AppProvider extends React.Component<{ children?: React.ReactChildren }, { 
       ? <Provider store={store} key="provider">
           {this.props.children}
         </Provider>
-      : <div>Loading...</div>
+      : <div>Loading...</div>;
   }
 }
 
-export default AppProvider
+export default AppProvider;
