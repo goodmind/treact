453678ca77409fbb4712bbba11743deb1353d79a
goodmind <andwebar@gmail.com>
2017-02-23 15:24:48 +0500

    refactor(AppProvider): remove pool connection

--- src/AppProvider.tsx
+++ src/AppProvider.tsx
@@ -2,8 +2,6 @@ import * as React from 'react';
 import { Provider } from 'react-redux';
 import { persistStore } from 'redux-persist';
 import { browserHistory } from 'react-router';
-
-import pool from 'helpers/Telegram/pool';
 import { configureStore } from './app/redux/store';
 
 export const store = configureStore(
@@ -11,16 +9,13 @@ export const store = configureStore(
   window.__INITIAL_STATE__,
 );
 
-
-
 class AppProvider extends React.Component<{ children?: React.ReactChildren }, { rehydrated: boolean }> {
   public state = { rehydrated: false };
 
   public componentWillMount() {
     const onDone = () => this.setState({ rehydrated: true });
-    const onConnect = () => persistStore(store,
+    persistStore(store,
       {whitelist: ['authKey', 'currentUser', 'currentDc']}, onDone);
-    pool.dcConnect().then(onConnect);
   }
 
   public render() {
