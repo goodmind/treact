83d3aac19e15ea5aaff1f10abd5c9224bf653f49
goodmind <andwebar@gmail.com>
2017-02-23 15:46:02 +0500

    refactor(Telegram/config): move app settings

--- src/app/helpers/Telegram/config.ts
+++ src/app/helpers/Telegram/config.ts
@@ -1,5 +1,16 @@
-import { restoreAuthKeyFromLocalStorage } from './helpers';
+// import { restoreAuthKeyFromLocalStorage } from './helpers';
 import { APP_ID, APP_HASH, SERVER, DEFAULT_DC_ID } from './constants';
+
+export const appSettings = {
+  invokeWithLayer: 0xda9b0d0d,
+  layer          : 57,
+  initConnection : 0x69796de9,
+  api_id         : APP_ID,
+  app_version    : '1.0.1',
+  lang_code      : 'en',
+};
+
+export const serverConfig = {};
 
 export const config = {
   // NOTE: if you FORK the project you MUST use your APP ID.
@@ -7,11 +18,7 @@ export const config = {
   // You can obtain your own APP ID for your application here: https://my.telegram.org
   id: APP_ID,
   hash: APP_HASH,
-  version: '1.0.1',
-  langCode: 'en',
-  deviceModel: '',
-  systemVersion: '',
-  authKey: restoreAuthKeyFromLocalStorage(),
+  // authKey: restoreAuthKeyFromLocalStorage(),
 };
 
 export { SERVER, APP_ID, APP_HASH, DEFAULT_DC_ID }
