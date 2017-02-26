57edcf7efa96dd657b5232c65a3d206850353bdb
goodmind <andwebar@gmail.com>
2017-02-27 03:07:26 +0500

    refactor(ChatList): add infinite scroll handlers

--- src/app/components/ChatList/index.tsx
+++ src/app/components/ChatList/index.tsx
@@ -1,10 +1,12 @@
 import * as React from 'react';
 import { ChatListSearch } from 'components';
+import * as InfiniteScroll from 'react-infinite-scroller';
 
 const style = require('./style.css');
 
 interface IProps {
   loading: boolean;
+  loadMore: Function;
 }
 
 const LoadingPane = () => (
@@ -15,15 +17,25 @@ const LoadingPane = () => (
 
 class ChatList extends React.Component<IProps, {}> {
   public render() {
-    const { loading, children } = this.props;
+    const { loading, children, loadMore } = this.props;
+
+    console.debug('ChatList', loadMore.toString());
 
     return (
       <div className={style.chatlist}>
         <ChatListSearch />
-        <div className={style.chatbox}>
-          {loading
-            ? <LoadingPane />
-            : children}
+        <div className={style.chatbody}>
+          <InfiniteScroll
+            className={style.box}
+            pageStart={0}
+            loadMore={loadMore}
+            initialLoad={false}
+            hasMore={true}
+            useWindow={false}>
+            {loading
+              ? <LoadingPane />
+              : children}
+          </InfiniteScroll>
         </div>
       </div>
     );
