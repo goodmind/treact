bee4cf7cbf0aac09faf069feb1abc33e7c6d4a10
goodmind <andwebar@gmail.com>
2017-03-11 18:36:38 +0500

    refactor(histories): handle sent message

--- src/app/redux/modules/histories.ts
+++ src/app/redux/modules/histories.ts
@@ -1,10 +1,11 @@
 import { createReducer } from 'redux-act';
 import { IStoreList } from 'helpers/state';
-import { CHATS } from 'actions';
+import { CHATS, MESSAGES } from 'actions';
 
 import { updateStoreListSorted, modelDefaults } from 'helpers/reselector';
 
 const { LOAD_SLICE, GET_DIALOGS } = CHATS;
+const { SEND_TEXT } = MESSAGES;
 
 export type IStoreHistory = number[];
 
@@ -13,6 +14,7 @@ export type IStoreHistories = IStoreList<IStoreHistory>;
 const updater = updateStoreListSorted('histories');
 
 const newReducer = createReducer({
+  [SEND_TEXT.DONE]: updater,
   [LOAD_SLICE.DONE]: updater,
   [GET_DIALOGS.DONE]: updater,
 }, modelDefaults);
