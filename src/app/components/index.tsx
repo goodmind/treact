31e48b35e4e848230fd5f25252d5929bec1c9077
goodmind <andwebar@gmail.com>
2017-03-21 22:35:59 +0500

    refactor(components): re-export components

--- src/app/components/index.tsx
+++ src/app/components/index.tsx
@@ -1,3 +1,4 @@
+export { App } from './App';
 export { Header } from './Header';
 export { Chat } from './Chat';
 export { ChatList } from './ChatList';
@@ -5,3 +6,4 @@ export { ChatListItem } from './ChatListItem';
 export { ChatListSearch } from './ChatListSearch';
 export { ChatMessage } from './ChatMessage';
 export { AutoSizeTextarea } from './AutoSizeTextarea';
+export { InstantMessages } from './InstantMessages';
