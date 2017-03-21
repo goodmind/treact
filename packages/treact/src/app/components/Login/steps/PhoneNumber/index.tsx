e3efe68e61044f64186b14bf8238d9ea7b47e2e7
goodmind <andwebar@gmail.com>
2017-03-21 22:35:29 +0500

    refactor(PhoneNumber): move PhoneNumber from container

---
+++ packages/treact/src/app/components/Login/steps/PhoneNumber/index.tsx
@@ -0,0 +1,29 @@
+import * as React from 'react';
+
+const s = require('./style.css');
+const t = require('../../style.css');
+
+const PhoneNumber = ({ error, nextStep, change, phoneCode }) => (
+  <div className={t.loginStep}>
+    <h1>Your Phone</h1>
+    <p>
+      Please confirm your country code and enter your phone number.
+    </p>
+    <div className={`row ${t.formGroupLogin}`}>
+      <input
+        onChange={change}
+        name="phoneCode"
+        className={`${s.phoneCode} col-xs-2 form-control form-control-lg`}
+        value={phoneCode} type="tel" />
+      <input
+        onChange={change}
+        name="phoneNumber"
+        className="col-xs-10 form-control form-control-lg"
+        placeholder="--- --- -- --" type="tel" />
+    </div>
+    {error && <div>Error type: {error.description}</div>}
+    <button onClick={nextStep} className={`${t.btn} ${t.primary}`}>Next</button>
+  </div>
+);
+
+export { PhoneNumber }
