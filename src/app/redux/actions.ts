04bed34cc4f2cd1916483b953ee3e4a4b6d10968
goodmind <andwebar@gmail.com>
2017-06-15 23:04:40 +0500

    refactor(actions): restrict types

--- src/app/redux/actions.ts
+++ src/app/redux/actions.ts
@@ -1,16 +1,17 @@
 import { createAction } from 'redux-act';
+import { IAuthError } from 'redux/modules/auth';
 
-export const actionEvent = (reduxMessage: string) => ({
-  INIT: createAction(`[begin] ${reduxMessage}`),
-  DONE: createAction(`[end] ${reduxMessage}`),
-  FAIL: createAction(`[fail] ${reduxMessage}`),
+export const actionEvent = <I, D, F>(reduxMessage: string) => ({
+  INIT: createAction<I, {}>(`[begin] ${reduxMessage}`),
+  DONE: createAction<D, {}>(`[end] ${reduxMessage}`),
+  FAIL: createAction<F, {}>(`[fail] ${reduxMessage}`),
 });
 
 export const AUTH = {
-  SEND_CODE: actionEvent('send code'),
-  SIGN_IN: actionEvent('sign in'),
-  GET_PASSWORD: actionEvent('get password'),
-  LOG_OUT: actionEvent('user log out'),
+  SEND_CODE   : actionEvent<{}, {}, IAuthError>('send code'),
+  SIGN_IN     : actionEvent<{}, {}, IAuthError>('sign in'),
+  GET_PASSWORD: actionEvent<{}, {}, IAuthError>('get password'),
+  LOG_OUT     : actionEvent<{}, {}, IAuthError>('user log out'),
 };
 
 export const MESSAGES = {
@@ -22,13 +23,13 @@ export const UPDATES = {
 };
 
 export const CHATS = {
-  LOAD_SLICE: actionEvent('load slice'),
+  LOAD_SLICE : actionEvent('load slice'),
   GET_DIALOGS: actionEvent('load every dialog slice'),
-  SELECT: createAction('select dialog by id'),
+  SELECT     : createAction('select dialog by id'),
 };
 
 export const CACHE = {
   QUEUE: createAction('add imgs to download queue'),
-  LOAD: createAction('load next img'),
-  DONE: createAction('complete downloading img'),
+  LOAD : createAction<number[], {}>('load next img'),
+  DONE : createAction('complete downloading img'),
 };
