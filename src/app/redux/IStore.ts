d71ea0032260104899ca72e8a075f473f90c45d7
goodmind <andwebar@gmail.com>
2017-06-15 23:06:31 +0500

    refactor(IStore): sort imports

--- src/app/redux/IStore.ts
+++ src/app/redux/IStore.ts
@@ -2,17 +2,19 @@ import { Dispatch } from 'redux';
 // NOTE This type coming from redux-thunk module, not from redux itself
 
 import { IAuth } from './modules/auth';
-import { IStoreHistories } from './modules/histories';
-import { IStoreDialogs } from './modules/dialogs';
-import { IStoreUsers } from './modules/users';
-import { IStorePeers } from './modules/peers';
 import { IStoreChats } from './modules/chats';
-import { IStoreLoadings } from './modules/loadings';
-import { IStoreSelected } from './modules/selected';
+import { IStoreDialogs } from './modules/dialogs';
 import { StatusStore } from './modules/files';
+import { IStoreHistories } from './modules/histories';
+import { IStoreLoadings } from './modules/loadings';
+import { IStoreMessages } from './modules/messages';
+import { IStorePeers } from './modules/peers';
+import { IStorePhotos } from './modules/photos';
+import { IStoreSelected } from './modules/selected';
+import { IStoreUsers } from './modules/users';
 
 import { IStoreList } from 'helpers/state';
-import { IMtpFileLocation } from 'redux/mtproto';
+import { IMtpFileLocation, IMtpUser } from 'redux/mtproto';
 export interface IStore {
   authKey: any;
   auth: IAuth;
