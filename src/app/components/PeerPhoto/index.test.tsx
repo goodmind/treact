9c22dd4f2b291c2b9bd99533bc9dfa7eac123d83
goodmind <andwebar@gmail.com>
2017-06-23 03:06:53 +0500

    test(PeerPhoto): add snapshot test

---
+++ src/app/components/PeerPhoto/index.test.tsx
@@ -0,0 +1,22 @@
+import * as React from 'react';
+import * as renderer from 'react-test-renderer';
+import { PeerPhoto, PeerPhotoEmpty } from './';
+
+jest.mock('helpers/FileManager/picStore');
+
+it('PeerPhoto renders correctly', () => {
+  const tree = renderer.create(
+    <PeerPhoto id={1} className="" />,
+  ).toJSON();
+
+  expect(tree).toMatchSnapshot();
+});
+
+it('PeerPhotoEmpty renders correctly', () => {
+  const tree = renderer.create(
+    <PeerPhotoEmpty className="" />,
+  ).toJSON();
+
+  expect(tree).toMatchSnapshot();
+});
+
