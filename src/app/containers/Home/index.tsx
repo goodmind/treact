2708e68411dd1f62387a84080cacc17dc22f99c6
goodmind <andwebar@gmail.com>
2017-07-09 21:14:56 +0500

    refactor(Home): use standalone history

--- src/app/containers/Home/index.tsx
+++ src/app/containers/Home/index.tsx
@@ -1,7 +1,8 @@
 import * as React from 'react';
 import { connect } from 'react-redux';
-import { push } from 'react-router-redux';
 import { IDispatch, IStore } from 'redux/IStore';
+// TODO: use absolute paths
+import history from '../../../history';
 
 type IConnectedState = Pick<IStore, 'auth'>;
 type IConnectedActions = { dispatch: IDispatch };
@@ -9,12 +10,12 @@ type IOwnProps = {};
 type IProps = IConnectedState & IConnectedActions & IOwnProps;
 
 class HomeImpl extends React.Component<IProps, {}> {
-  public componentDidMount() {
-    const { auth, dispatch } = this.props;
+  public componentWillMount() {
+    const { auth } = this.props;
     const route = auth.authenticated
       ? '/im'
       : '/login';
-    return dispatch(push(route));
+    return history.push(route);
   }
 
   public render() {
