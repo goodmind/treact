f294fbba3090a59e8502d241c9311cfcc7239fa5
goodmind <andwebar@gmail.com>
2017-07-20 18:38:07 +0500

    refactor(AppContainer): update imports affected by Header splitting

--- packages/treact/src/app/containers/App/index.tsx
+++ packages/treact/src/app/containers/App/index.tsx
@@ -1,4 +1,5 @@
-import { App, Header } from 'components';
+import { App } from 'components';
+import { Header } from 'containers';
 import * as React from 'react';
 import * as Helmet from 'react-helmet';
 import * as appConfig from '../../../../config/main';
