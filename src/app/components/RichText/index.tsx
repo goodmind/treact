4acad94ff86914a6f5deea71728cc57e527b717b
goodmind <andwebar@gmail.com>
2017-07-27 17:50:07 +0500

    refactor(RichText): use components for entities

--- src/app/components/RichText/index.tsx
+++ src/app/components/RichText/index.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import { Entity, parse } from './parser';
+import { Entity, parse, RichText } from './parser';
 
 export { Entity };
 
@@ -10,33 +10,33 @@ interface Props {
 }
 
 // TODO: better place for this?
-const transformers: { [key: string]: (t: string) => JSX.Element } = {
-  messageEntityUnknown    : t => <span className="unknown">{t}</span>,
-  messageEntityMention    : t => <span className="mention">{t}</span>,
-  messageEntityHashtag    : t => <span className="hashtag">{t}</span>,
-  messageEntityBotCommand : t => <span className="botcommand">{t}</span>,
-  messageEntityUrl        : t => <a href={t}>{t}</a>,
-  messageEntityEmail      : t => <a href={`mailto:${t}`}>{t}</a>,
-  messageEntityBold       : t => <strong>{t}</strong>,
-  messageEntityItalic     : t => <em>{t}</em>,
-  messageEntityCode       : t => <code>{t}</code>,
-  messageEntityPre        : t => <pre>{t}</pre>,
-  messageEntityTextUrl    : t => <span className="texturl">{t}</span>,
-  messageEntityMentionName: t => <span className="mentionname">{t}</span>,
+const transformers: { [key: string]: (props: RichText) => JSX.Element } = {
+  messageEntityUnknown    : ({ text }) => <span className="unknown">{text}</span>,
+  messageEntityMention    : ({ text }) => <span className="mention">{text}</span>,
+  messageEntityHashtag    : ({ text }) => <span className="hashtag">{text}</span>,
+  messageEntityBotCommand : ({ text }) => <span className="botcommand">{text}</span>,
+  messageEntityUrl        : ({ text }) => <a href={text}>{text}</a>,
+  messageEntityEmail      : ({ text }) => <a href={`mailto:${text}`}>{text}</a>,
+  messageEntityBold       : ({ text }) => <strong>{text}</strong>,
+  messageEntityItalic     : ({ text }) => <em>{text}</em>,
+  messageEntityCode       : ({ text }) => <code>{text}</code>,
+
+  messageEntityPre        : ({ text }) => <pre>{text}</pre>,
+  messageEntityTextUrl    : ({ text, url }: RichText & { url: string }) => <a href={url}>{text}</a>,
+  messageEntityMentionName: ({ text, user_id }: RichText & { user_id: number }) => <a href={`#${user_id}`}>{text}</a>,
 
   // Custom entity
-  messageEntityText       : t => <span>{t}</span>,
+  messageEntityText       : ({ text }) => <span>{text}</span>,
 };
 
 const RichText = ({ id, entities, text }: Props) => {
   const formattedText = parse(entities, text);
-
   return (
     <span>
-      {formattedText.map((p, i) =>
-        React.cloneElement(
-          transformers[p._](p.text),
-          { key: `rt${id}_${i}` }))}
+      {formattedText.map((p, i) => {
+        const Entity = transformers[p._];
+        return <Entity key={`rt${id}_${i}`} {...p} />;
+      })}
     </span>
   );
 };
