d27f86f7f21b8e450e4bf0fc5678f3053ec61747
goodmind <andwebar@gmail.com>
2017-07-31 21:24:31 +0500

    refactor(components): begin migration to glamorous jss

--- src/app/components/ChatListItem/index.tsx
+++ src/app/components/ChatListItem/index.tsx
@@ -1,5 +1,6 @@
 import * as cx from 'classnames';
 import { PeerPhoto } from 'containers/PeerPhoto';
+import styled from 'glamorous';
 import * as React from 'react';
 import * as style from './style.css';
 
@@ -8,6 +9,7 @@ interface ChatListBasicProps {
   name: string;
   selected: boolean;
   click: React.MouseEventHandler<{}>;
+  children?: React.ReactNode;
 }
 
 interface ChatListFullProps extends ChatListBasicProps {
@@ -17,74 +19,98 @@ interface ChatListFullProps extends ChatListBasicProps {
   unreadCount: number;
 }
 
+const Counter = styled.div({
+  backgroundColor: '#bbb',
+  borderRadius: '19px',
+  fontSize: '12px',
+  fontWeight: 700,
+  minWidth: '9px',
+  padding: '1px 5px',
+  textAlign: 'center',
+  [`.${style.active} &`]: {
+    backgroundColor: '#c6e1f7',
+  },
+});
+
+const StyledUnreadBadge = styled.div<UnreadProps>(props => ({
+  display: props.unread ? 'block' : 'none',
+  color: '#fff',
+  [`.${style.active} &`]: {
+    color: '#419fd9',
+  },
+}));
+
+const StyledSenderPreview = styled.div<SenderProps>(props => ({
+  display: props.userName ? 'inline-block' : 'none',
+  color: '#538bb4',
+  paddingRight: '4px',
+  [`.${style.active} &`]: {
+    color: '#fff',
+  },
+}));
+
+const StyledMessagePreview = styled.div({
+  color: '#888888',
+  flex: 1,
+  textOverflow: 'ellipsis',
+  overflow: 'hidden',
+  whiteSpace: 'nowrap',
+  [`.${style.active} &`]: {
+    color: '#fff',
+  },
+});
+
 type UnreadProps = { unread: number };
-const UnreadBadge = ({ unread }: UnreadProps) => {
-  const block = cx({
-    [style.unread]: true,
-    [style.hidden]: unread === 0,
-  });
-  return (
-    <div className={block}>
-      <div className={style.counter}>{unread}</div>
-    </div>
-  );
-};
+const UnreadBadge = ({ unread }: UnreadProps) => (
+  <StyledUnreadBadge unread={unread}>
+    <Counter>{unread}</Counter>
+  </StyledUnreadBadge>
+);
 
 type SenderProps = { userName: string };
-const SenderPreview = ({ userName }: SenderProps) => {
-  const sender = cx({
-    [style.sender]: true,
-    [style.hidden]: !userName,
-  });
-  return (
-    <div className={sender}>
-      <span>{userName}</span>
-      <span>:</span>
-    </div>
-  );
-};
+const SenderPreview = ({ userName }: SenderProps) => (
+  <StyledSenderPreview userName={userName}>
+    <span>{userName}</span>
+    <span>:</span>
+  </StyledSenderPreview>
+);
 
 type MessageProps = { text: string, userName: string, isYou: boolean };
 const MessagePreview = ({ text, userName, isYou }: MessageProps) => (
-  <div className={style.message}>
+  <StyledMessagePreview>
     <SenderPreview userName={isYou ? 'You' : userName} />
     <span>{text}</span>
-  </div>
+  </StyledMessagePreview>
 );
 
-// <PeerPhoto peerID={id} />
-
-export class ChatListItemEmpty extends React.Component<ChatListBasicProps, {}> {
-  public render() {
-    const {
-      id,
-      name,
-      click,
-      selected,
-      children,
-    } = this.props;
-    const block = cx({
-      [style.item]: true,
-      [style.active]: selected,
-    });
-    return (
-      <div
-        onClick={click}
-        className={block}>
-        <PeerPhoto peerID={id} />
-        <div className={style.info}>
-          <div className={style.top}>
-            <div className={style.chatname}>{name}</div>
-            <div className={style.time}>00:00</div>
-          </div>
-          <div className={style.bottom}>
-            {children}
-          </div>
+export const ChatListItemEmpty = ({
+  id,
+  name,
+  click,
+  selected,
+  children,
+}: ChatListBasicProps) => {
+  const block = cx({
+    [style.item]: true,
+    [style.active]: selected,
+  });
+  return (
+    <div
+      onClick={click}
+      className={block}>
+      <PeerPhoto peerID={id} />
+      <div className={style.info}>
+        <div className={style.top}>
+          <div className={style.chatname}>{name}</div>
+          <div className={style.time}>00:00</div>
+        </div>
+        <div className={style.bottom}>
+          {children}
         </div>
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
 
 export const ChatListItem = ({
   id, name, click, selected,
