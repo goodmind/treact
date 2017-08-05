a4ff8c68282da242d390c6308c12753a5a6f3ba0
goodmind <andwebar@gmail.com>
2017-08-06 03:35:43 +0500

    refactor(Chat): replace more classes with styled components

--- packages/treact/src/app/components/Chat/index.tsx
+++ packages/treact/src/app/components/Chat/index.tsx
@@ -1,12 +1,30 @@
 import { InfiniteScroll } from 'components';
 import { ChatFooter } from 'containers';
+import styled from 'glamorous';
 import * as React from 'react';
 import * as s from './style.css';
 
+const StyledChat = styled.div(({ theme }) => ({
+  backgroundImage: `url(${theme.__backgroundImage__})`,
+  backgroundSize: 'cover',
+  display: 'flex',
+  flex: 65,
+  height: '100%',
+}));
+
+const Header = styled.div(({ theme }) => ({
+  backgroundColor: theme.topBarBg,
+  borderBottom: `1px solid ${theme.shadowFg}`,
+  display: 'flex',
+  height: '54px',
+  paddingLeft: '16px',
+  borderLeft: `1px solid ${theme.shadowFg}`,
+}));
+
 export const DefaultScreen = () => (
-  <div className={s.chat}>
+  <StyledChat>
     <div className={s.bubble}>Please select a chat to start messaging</div>
-  </div>
+  </StyledChat>
 );
 
 type BasicProps = {
@@ -15,13 +33,13 @@ type BasicProps = {
 };
 
 const ChatHeader = ({ name, userCount }: BasicProps) => (
-  <div className={s.chatheader}>
+  <Header>
     <div className={s.left}>
       <div className={s.top}>{name}</div>
       <div className={s.bottom}>{userCount} members</div>
     </div>
     <div className={s.right} />
-  </div>
+  </Header>
 );
 
 type Props = BasicProps & {
@@ -32,7 +50,7 @@ class Chat extends React.Component<Props, {}> {
   public render() {
     const { name, userCount, children, loadMore } = this.props;
     return (
-      <div className={s.chat}>
+      <StyledChat>
         <div className={s.chatcontainer}>
           <ChatHeader name={name} userCount={userCount} />
           <div className={s.chatbody}>
@@ -49,7 +67,7 @@ class Chat extends React.Component<Props, {}> {
           </div>
           <ChatFooter />
         </div>
-      </div>
+      </StyledChat>
     );
   }
 }
