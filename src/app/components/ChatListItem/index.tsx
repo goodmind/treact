73ade0a6d7bcde302bcf8a55114c25b4eaa681e9
goodmind <andwebar@gmail.com>
2017-08-06 03:40:08 +0500

    refactor(ChatListItem): add type for theme prop to all components

--- src/app/components/ChatListItem/index.tsx
+++ src/app/components/ChatListItem/index.tsx
@@ -1,7 +1,7 @@
-import * as cx from 'classnames';
 import { PeerPhoto } from 'containers/PeerPhoto';
 import styled from 'glamorous';
 import * as React from 'react';
+import { Themeable } from 'themes/theme.h';
 import * as style from './style.css';
 
 interface ChatListBasicProps {
@@ -19,24 +19,26 @@ interface ChatListFullProps extends ChatListBasicProps {
   unreadCount: number;
 }
 
-const Counter = styled.div({
-  backgroundColor: '#bbb',
+const active = 'active';
+
+const Counter = styled.div<Themeable>(({ theme }) => ({
+  backgroundColor: theme.dialogsUnreadBg,
   borderRadius: '19px',
   fontSize: '12px',
   fontWeight: 700,
   minWidth: '9px',
   padding: '1px 5px',
   textAlign: 'center',
-  [`.${style.active} &`]: {
-    backgroundColor: '#c6e1f7',
+  [`.${active} &`]: {
+    backgroundColor: theme.dialogsUnreadBgActive,
   },
-});
+}));
 
-const StyledUnreadBadge = styled.div<UnreadProps>(props => ({
-  display: props.unread ? 'block' : 'none',
-  color: '#fff',
-  [`.${style.active} &`]: {
-    color: '#419fd9',
+const StyledUnreadBadge = styled.div<UnreadProps & Themeable>(({ unread, theme }) => ({
+  display: unread ? 'block' : 'none',
+  color: theme.dialogsUnreadFg,
+  [`.${active} &`]: {
+    color: theme.dialogsUnreadFgActive,
   },
 }));
 
@@ -44,7 +46,7 @@ const StyledSenderPreview = styled.div<SenderProps>(props => ({
   display: props.userName ? 'inline-block' : 'none',
   color: '#538bb4',
   paddingRight: '4px',
-  [`.${style.active} &`]: {
+  [`.${active} &`]: {
     color: '#fff',
   },
 }));
@@ -55,7 +57,7 @@ const StyledMessagePreview = styled.div({
   textOverflow: 'ellipsis',
   overflow: 'hidden',
   whiteSpace: 'nowrap',
-  [`.${style.active} &`]: {
+  [`.${active} &`]: {
     color: '#fff',
   },
 });
@@ -83,6 +85,45 @@ const MessagePreview = ({ text, userName, isYou }: MessageProps) => (
   </StyledMessagePreview>
 );
 
+type DialogItemProps = { active: boolean };
+const StyledDialogItem = styled.div<DialogItemProps & Themeable>({
+  height: '62px',
+  paddingRight: '10px',
+}
+, props => props.active ? active : {}
+, ({ theme }) => ({
+  backgroundColor: theme.dialogsBg,
+  ':hover': {
+    backgroundColor: theme.dialogsBgOver,
+  },
+  [`&.${active}, &.${active}:hover`]: {
+    backgroundColor: theme.dialogsBgActive,
+    marginRight: '-1px',
+  },
+}));
+
+const StyledPeerPhoto = styled(PeerPhoto)({
+  borderRadius: '50px',
+  float: 'left',
+  height: '46px',
+  width: '46px',
+  margin: '8px 10px',
+  marginRight: '12px',
+});
+
+const Name = styled.div<Themeable>(({ theme }) => ({
+  alignSelf: 'center',
+  color: theme.dialogsNameFg,
+  flex: '1 100%',
+  fontWeight: 600,
+  textOverflow: 'ellipsis',
+  overflow: 'hidden',
+  whiteSpace: 'nowrap',
+  [`.${active} &`]: {
+    color: theme.dialogsNameFgActive,
+  },
+}));
+
 export const ChatListItemEmpty = ({
   id,
   name,
@@ -90,25 +131,21 @@ export const ChatListItemEmpty = ({
   selected,
   children,
 }: ChatListBasicProps) => {
-  const block = cx({
-    [style.item]: true,
-    [style.active]: selected,
-  });
   return (
-    <div
-      onClick={click}
-      className={block}>
-      <PeerPhoto peerID={id} />
+    <StyledDialogItem
+      active={selected}
+      onClick={click}>
+      <StyledPeerPhoto peerID={id} />
       <div className={style.info}>
         <div className={style.top}>
-          <div className={style.chatname}>{name}</div>
+          <Name>{name}</Name>
           <div className={style.time}>00:00</div>
         </div>
         <div className={style.bottom}>
           {children}
         </div>
       </div>
-    </div>
+    </StyledDialogItem>
   );
 };
 
