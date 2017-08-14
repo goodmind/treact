af7cc26be1f5201a2e2ff221f256c88d7e79afb4
goodmind <andwebar@gmail.com>
2017-08-14 23:06:20 +0500

    refactor(HeaderContainer): add night mode switcher

--- src/app/containers/Header/index.tsx
+++ src/app/containers/Header/index.tsx
@@ -1,4 +1,5 @@
 import { logOut } from 'api/auth';
+import { switchNightMode } from 'api/themes';
 import { Header } from 'components';
 import { connect } from 'react-redux';
 import { Dispatch } from 'redux/store.h';
@@ -10,6 +11,9 @@ interface ConnectedActions {
 const dispatchToProps = (dispatch: Dispatch) => ({
   logOut() {
     dispatch(logOut());
+  },
+  switchNightMode() {
+    dispatch(switchNightMode());
   },
 });
 
