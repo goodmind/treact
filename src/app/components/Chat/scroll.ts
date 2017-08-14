57bfafadf58a765681eb8a8e6ad223c650e97e31
goodmind <andwebar@gmail.com>
2017-08-14 23:10:05 +0500

    refactor(Chat): make component themeable

---
+++ src/app/components/Chat/scroll.ts
@@ -0,0 +1,9 @@
+import { InfiniteScroll } from 'components/InfiniteScroll';
+import styled from 'glamorous';
+
+export default styled(InfiniteScroll)({
+  minHeight: 'min-content',
+  display: 'flex',
+  flexDirection: 'column',
+  flex: 1,
+});
