0209c57f58e2733818259e3b1863611632c75445
goodmind <andwebar@gmail.com>
2017-08-15 23:32:45 +0500

    refactor(Login): use theme colors

--- packages/treact/src/app/components/Login/index.tsx
+++ packages/treact/src/app/components/Login/index.tsx
@@ -1,13 +1,14 @@
 import styled from 'glamorous';
 
-export const Login = styled.div({
-  backgroundColor: '#FFF',
+export const Login = styled.div(({ theme }) => ({
+  backgroundColor: theme.introBg,
+  color: theme.introDescriptionFg,
   width: '100%',
   flex: '1 100%',
   display: 'flex',
   alignItems: 'center',
   justifyContent: 'center',
-});
+}));
 
 
 
