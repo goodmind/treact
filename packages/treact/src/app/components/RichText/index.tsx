85f311e8ae5c2745f8a70baf4a13b02adf8d0537
goodmind <andwebar@gmail.com>
2017-08-17 17:31:27 +0500

    style(RichText): indent a bit

--- packages/treact/src/app/components/RichText/index.tsx
+++ packages/treact/src/app/components/RichText/index.tsx
@@ -11,19 +11,41 @@ interface Props {
 
 // TODO: better place for this?
 const transformers: { [key: string]: (props: RichText) => JSX.Element } = {
-  messageEntityUnknown    : ({ text }) => <span className="unknown">{text}</span>,
-  messageEntityMention    : ({ text }) => <span className="mention">{text}</span>,
-  messageEntityHashtag    : ({ text }) => <span className="hashtag">{text}</span>,
-  messageEntityBotCommand : ({ text }) => <span className="botcommand">{text}</span>,
-  messageEntityUrl        : ({ text }) => <a href={text}>{text}</a>,
-  messageEntityEmail      : ({ text }) => <a href={`mailto:${text}`}>{text}</a>,
-  messageEntityBold       : ({ text }) => <strong>{text}</strong>,
-  messageEntityItalic     : ({ text }) => <em>{text}</em>,
-  messageEntityCode       : ({ text }) => <code>{text}</code>,
+  messageEntityUnknown    : ({ text }) =>
+    <span className="unknown">{text}</span>,
 
-  messageEntityPre        : ({ text }) => <pre>{text}</pre>,
-  messageEntityTextUrl    : ({ text, url }: RichText & { url: string }) => <a href={url}>{text}</a>,
-  messageEntityMentionName: ({ text, user_id }: RichText & { user_id: number }) => <a href={`#${user_id}`}>{text}</a>,
+  messageEntityMention    : ({ text }) =>
+    <span className="mention">{text}</span>,
+
+  messageEntityHashtag    : ({ text }) =>
+    <span className="hashtag">{text}</span>,
+
+  messageEntityBotCommand : ({ text }) =>
+    <span className="botcommand">{text}</span>,
+
+  messageEntityUrl        : ({ text }) =>
+    <a href={text}>{text}</a>,
+
+  messageEntityEmail      : ({ text }) =>
+    <a href={`mailto:${text}`}>{text}</a>,
+
+  messageEntityBold       : ({ text }) =>
+    <strong>{text}</strong>,
+
+  messageEntityItalic     : ({ text }) =>
+    <em>{text}</em>,
+
+  messageEntityCode       : ({ text }) =>
+    <code>{text}</code>,
+
+  messageEntityPre        : ({ text }) =>
+    <pre>{text}</pre>,
+
+  messageEntityTextUrl    : ({ text, url }: RichText & { url: string }) =>
+    <a href={url}>{text}</a>,
+
+  messageEntityMentionName: ({ text, user_id }: RichText & { user_id: number }) =>
+    <a href={`#${user_id}`}>{text}</a>,
 
   // Custom entity
   messageEntityText       : ({ text }) => <span>{text}</span>,
