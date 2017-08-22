4cbb71f18659f60f063389c1849901dc0e021884
goodmind <andwebar@gmail.com>
2017-08-22 17:00:01 +0500

    style(SuccessContainer): reorder imports

--- src/app/containers/Login/steps/Success/index.tsx
+++ src/app/containers/Login/steps/Success/index.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react';
 import { connect } from 'react-redux';
-import { Store, Dispatch } from 'redux/store.h';
+import { Dispatch, Store } from 'redux/store.h';
 
 type ConnectedState = Pick<Store, 'currentUser'>;
 type ConnectedActions = { dispatch: Dispatch };
