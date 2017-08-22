3ec4536ff6646fbe13b77f76bd5a45484b74fe4a
goodmind <andwebar@gmail.com>
2017-08-22 16:55:14 +0500

    fix(MediaContainer): show correct preview for documents

--- src/app/containers/Media/index.tsx
+++ src/app/containers/Media/index.tsx
@@ -11,17 +11,46 @@ import {
   MessageMediaVenue,
   MessageMediaWebPage,
 } from 'components/Media';
+import { denormalize } from 'normalizr';
+import { map, prop } from 'ramda';
 import * as React from 'react';
-import { MtpMessage } from 'redux/mtproto';
+import { connect } from 'react-redux';
+import { media as schema } from 'redux/api/chatList';
+import { MtpMessage, MtpMessageMedia, TMtpMessageMediaRecord } from 'redux/mtproto';
+import { Store } from 'redux/store.h';
 
-const mappings: { [key: string]: [React.StatelessComponent<Props['media']>, string] } = {
+import { TLDocument } from 'helpers/reselector.h';
+
+type StoredMedia = {
+  [K in keyof TMtpMessageMediaRecord]: TMtpMessageMediaRecord[K]
+} & {
+  messageMediaDocument: { document: TLDocument },
+};
+
+type Mappings = {
+  [K in keyof TMtpMessageMediaRecord]: [
+    React.StatelessComponent<MtpMessageMedia>,
+    string | ((media: StoredMedia[K]) => string)
+  ];
+};
+
+// TODO: move preview to another component or not?
+const mappings: Mappings = {
   messageMediaEmpty: [MessageMediaEmpty, 'Empty Message'],
   messageMediaGeo: [MessageMediaGeo, 'Location'],
   messageMediaContact: [MessageMediaContact, 'Contact'],
   messageMediaUnsupported: [MessageMediaUnsupported, 'Unsupported message'],
   messageMediaVenue: [MessageMediaVenue, 'Venue'],
   messageMediaPhoto: [MessageMediaPhoto, 'Photo'],
-  messageMediaDocument: [MessageMediaDocument, 'Document'],
+  messageMediaDocument: [MessageMediaDocument, ({ document }) => ({
+    'sticker' : 'Sticker',
+    'gif'     : 'GIF',
+    'round'   : 'Video message',
+    'audio'   : 'Audio',
+    'voice'   : 'Voice message',
+    'video'   : 'Video',
+    'document': document.file_name,
+  }[document.type])],
   messageMediaWebPage: [MessageMediaWebPage, 'Webpage'],
   messageMediaGame: [MessageMediaGame, 'Game'],
   messageMediaInvoice: [MessageMediaInvoice, 'Invoice'],
@@ -29,6 +58,8 @@ const mappings: { [key: string]: [React.StatelessComponent<Props['media']>, stri
 
 type Props = Pick<MtpMessage, 'media'> & { preview?: boolean };
 const Media = ({ media, preview }: Props) => {
+  console.error(media);
+
   const [Attachment, Preview] = mappings[media._];
   const text = typeof Preview === 'function'
     ? Preview(media)
@@ -39,4 +70,9 @@ const Media = ({ media, preview }: Props) => {
     : React.createElement(Attachment, media);
 };
 
-export { Media };
+const mapStateToProps = (state: Store, { media }: Props) => {
+  const entities = map(prop('byId'), state);
+  return { media: denormalize(media, schema, entities) };
+};
+
+export default connect(mapStateToProps)(Media);
