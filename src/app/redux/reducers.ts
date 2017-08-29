64ef8ba1d82166817f6c37996e6834c7502f626b
goodmind <andwebar@gmail.com>
2017-08-29 22:09:10 +0500

    refactor(reducers): add media, settings, avatars reducers

--- src/app/redux/reducers.ts
+++ src/app/redux/reducers.ts
@@ -1,6 +1,7 @@
 import { combineReducers } from 'redux';
 import { authReducer } from './modules/auth';
 import { authKeyReducer } from './modules/authKey';
+import avatars from './modules/avatars';
 import chats from './modules/chats';
 import { currentDcReducer } from './modules/currentDc';
 import { currentUserReducer } from './modules/currentUser';
@@ -9,10 +10,12 @@ import documents from './modules/documents/index';
 import files from './modules/files';
 import histories from './modules/histories';
 import loadings from './modules/loadings';
+import media from './modules/media';
 import messages from './modules/messages';
 import peers from './modules/peers';
 import photos from './modules/photos';
 import selected from './modules/selected';
+import settings from './modules/settings';
 import theme from './modules/theme';
 import users from './modules/users';
 import { Store } from './store.h';
@@ -31,10 +34,14 @@ const rootReducer = combineReducers<Store>({
   loadings,
   selected,
   messages,
-  documents,
+  settings,
   files,
-  photos,
+  avatars,
   theme,
+
+  media,
+  documents,
+  photos,
 });
 
 export default rootReducer;
