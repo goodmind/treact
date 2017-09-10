eb681844b618cb64c5efc7a5c316232eb4148f10
goodmind <andwebar@gmail.com>
2017-09-10 11:58:38 +0500

    refactor(types/universal-router): use property-based API for user-defined types

--- packages/treact/config/types/universal-router.d.ts
+++ packages/treact/config/types/universal-router.d.ts
@@ -1,4 +1,33 @@
 declare module 'universal-router' {
+  type Obj<T> = { [k: string]: T };
+  type Bool = '0'|'1';
+  type Not<T extends Bool> = { '1': '0'; '0': '1'; }[T];
+  type And<
+    A extends Bool,
+    B extends Bool
+  > = ({ 1: { 1: '1' } & Obj<'0'> } & Obj<Obj<'0'>>)[A][B];
+  type Indeterminate<T extends string> = And<
+    UnionHasKey<T, '0'>,
+    UnionHasKey<T, '1'>
+  >;
+  type Determinate<T extends Bool> = Not<Indeterminate<T>>;
+  type DefinitelyYes<T extends Bool> = And<T, Determinate<T>>;
+  type UnionHasKey<Union extends string, K extends string> = ({[S in Union]: '1' } & Obj<'0'>)[K];
+  type If<Cond extends '0'|'1', Then, Else> = { 1: Then, 0: Else }[Cond];
+  type ObjectHasKey<
+    O extends {},
+    K extends string
+  > = UnionHasKey<keyof O, K>;
+  type Keyed<T> = {[K in keyof T]: K };
+  type KeyedSafe<T> = Keyed<T> & Obj<never>;
+  type UnionToObject<Keys extends string> = { [K in Keys]: K };
+  type IntersectionUnions<Big extends string, Small extends string> = KeyedSafe<UnionToObject<Small>>[Big];
+  type UnionContained<T extends string, U extends string> = DefinitelyYes<({ [P in U]: '1' } & Obj<'0'>)[T | U]>;
+  type UnionEmpty<T extends string> =　And<UnionContained<T, 'foo'>, UnionContained<T, 'bar'>>;
+  type UnionsOverlap<Big extends string, Small extends string> = Not<UnionEmpty<IntersectionUnions<Big, Small>>>;
+  type ObjectHasKeySafe<O extends object, K extends string> = UnionsOverlap<keyof O, K>;
+  type ObjectProp<O extends Obj<any>, K extends string, Default = never> = If<ObjectHasKeySafe<O, K>, O[K], Default>;
+
   export default class Router {
     constructor(routes: Route, options?: Partial<Options>);
     constructor(routes: Route[], options?: Partial<Options>);
@@ -9,8 +38,10 @@ declare module 'universal-router' {
 
   export interface ActionResult {}
 
+  type ActionResultType = ObjectProp<ActionResult, 'type', any>;
+
   interface NextFunction {
-    (resume?: boolean, parent?: Route): Promise<ActionResult>
+    (resume?: boolean, parent?: Route): Promise<ActionResultType>
   }
 
   export interface Context {
@@ -30,7 +61,7 @@ declare module 'universal-router' {
     name?: string;
     parent?: any;
     children?: Route[];
-    action?: (context: Context, params: Params) => Promise<ActionResult>;
+    action?: (context: Context, params: Params) => Promise<ActionResultType>;
   }
 
   export interface Options {
