ff97c6a4de1e62a948fc79532ed0ca0771848774
goodmind <andwebar@gmail.com>
2017-09-15 22:19:40 +0500

    refactor(helpers/reselector.h): add specific document types

--- src/app/helpers/reselector.h.ts
+++ src/app/helpers/reselector.h.ts
@@ -7,6 +7,7 @@ import {
   MtpUser,
   TMtpMessageMediaRecord,
 } from 'redux/mtproto';
+import { ObjectOverwrite } from 'typelevel-ts';
 export type Payload<T> = {
   entities: {
     [K in keyof T]: {
@@ -24,17 +25,15 @@ export type TLAvatar = {
   photo_big: number;
 };
 
-export type TLDocument = ({
-  type: 'voice' | 'audio',
-  duration: number,
-  audioTitle?: string,
-  audioPerformer?: string,
-} | {
-  type: 'round' | 'video',
-  duration: number,
+export type TLAttrImageSize = {
   w: number,
   h: number,
-} | {
+};
+export type TLAttrFileName = { file_name: string };
+export type TLAttrDocument = {
+  type: 'document',
+};
+export type TLAttrSticker = {
   type: 'sticker',
   sticker: boolean,
   emoji: string,
@@ -42,15 +41,44 @@ export type TLDocument = ({
   stickerset: any,
   // tslint:disable-next-line
   stickerSetInput: any,
-} | {
+};
+export type TLAttrGIF = {
   type: 'gif',
   animated: boolean,
-} | {
-  type: 'document',
-  file_name: string,
-}) & MtpDocument & {
-  file_name: string,
 };
+export type TLAttrVoice = {
+  type: 'voice',
+  duration: number,
+};
+export type TLAttrAudio = {
+  type: 'audio',
+  duration: number,
+  audioTitle?: string,
+  audioPerformer?: string,
+};
+export type TLAttrRound = {
+  type: 'round',
+  duration: number,
+  w: number,
+  h: number,
+};
+export type TLAttrVideo = {
+  type: 'video',
+  duration: number,
+  w: number,
+  h: number,
+};
+export type TLDocumentRecord = {
+  audio: TLAttrAudio,
+  voice: TLAttrVoice,
+  round: TLAttrRound,
+  video: TLAttrVideo,
+  sticker: TLAttrSticker & TLAttrImageSize,
+  gif: TLAttrGIF & TLAttrImageSize,
+  document: TLAttrDocument,
+};
+export type TLDocument<T = TLDocumentRecord> =
+  T[keyof T] & MtpDocument & TLAttrFileName;
 
 export type TLMediaTypes = {
   messageMediaEmpty: 'empty',
@@ -64,11 +92,22 @@ export type TLMediaTypes = {
   messageMediaGame: 'game';
   messageMediaInvoice: 'invoice';
 };
-export type TLMediaRecord =
-  TMtpMessageMediaRecord
-  & {
+export type TLMediaDocumentRecord = {
+  [K in TLDocument['type']]:
+    TMtpMessageMediaRecord['messageMediaDocument']
+    & {
+      type: K,
+      document: TLDocumentRecord[K] & MtpDocument & TLAttrFileName,
+    }
+};
+export type TLMediaRecord = ObjectOverwrite<
+  TMtpMessageMediaRecord & {
     [K in keyof TLMediaTypes]: { type: TLMediaTypes[K] }
-  };
+  },
+  {
+    messageMediaDocument: TLMediaDocumentRecord[TLDocument['type']],
+  }
+>;
 export type TLMedia<T = TLMediaRecord> = T[keyof T];
 
 export type Slice = {
@@ -85,6 +124,7 @@ export type Slice = {
   // TODO: add types
   photos: {},
   photoSizes: {},
+  photoCachedSizes: {},
 };
 
 export type SlicePayload = Payload<Slice>;
