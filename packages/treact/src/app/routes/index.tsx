2ec9e9292b6fd663ae939774fbdda7a1a59b8485
goodmind <andwebar@gmail.com>
2017-09-28 00:32:46 +0500

    refactor(routes): remove app container from route resolver

--- packages/treact/src/app/routes/index.tsx
+++ packages/treact/src/app/routes/index.tsx
@@ -1,11 +1,11 @@
 import { fetchChatList } from 'api/chatList';
+import { api } from 'helpers/Telegram/pool';
 import * as React from 'react';
 import { Context, Params, Route } from 'universal-router';
 import history from '../../history';
 
 export const resolveRoute = async (context: Context, params: Params) => {
   const { route, store } = context;
-  const { App } = await import (/* webpackChunkName: "app" */ 'containers/App');
   const { auth } = store.getState();
 
   if (typeof route.action === 'function') {
@@ -15,18 +15,31 @@ export const resolveRoute = async (context: Context, params: Params) => {
     }
 
     const Comp = await route.action(context, params);
-    return <App><Comp/></App>;
+    return <Comp/>;
   }
 
   return null;
 };
 
-const routes: Route[] = [
+const routes: [Route] = [
   {
-    path: '/',
+    path: '',
+    // TODO: remove this
+    async action({ next }): Promise<React.ComponentType<{}>> {
+      // TODO: remove this
+      // tslint:disable-next-line
+      const Child = await next();
+      window.api = api;
+      await api('help.getConfig', {})
+        .then(app => console.debug('init', app))
+        .catch(err => console.error('init failed', err));
+
+      return function Middleware() { return Child; };
+    },
     children: [
       {
-        path: '/',
+        path: '',
+        name: 'home',
         guard: false,
         async action() {
           const { Home } = await import (/* webpackChunkName: "home" */ 'containers/Home');
@@ -39,7 +52,8 @@ const routes: Route[] = [
         guard: true,
         children: [
           {
-            path: '/',
+            path: '',
+            name: 'im',
             guard: true,
             async action({ store: { dispatch } }) {
               const { InstantMessages } = await import (/* webpackChunkName: "im" */ 'containers/InstantMessages');
@@ -49,6 +63,7 @@ const routes: Route[] = [
           },
           {
             path: '/:id',
+            name: 'im/user',
             guard: true,
             action: async (context, { id }: { id: number }) => (props: {}) => {
               console.debug('im route', id, props, context);
@@ -60,6 +75,7 @@ const routes: Route[] = [
 
       {
         path: '/login',
+        name: 'login',
         guard: false,
         async action() {
           const { Login } = await import (/* webpackChunkName: "login" */ 'containers/Login');
