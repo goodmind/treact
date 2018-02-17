842fad5491e5a370bdb977c2f2881ebd36c477d9
andretshurotshka <andwebar@gmail.com>
2018-02-17 06:51:24 +0500

    refactor(Telegram/config): update schema

--- packages/treact/src/app/helpers/Telegram/config.ts
+++ packages/treact/src/app/helpers/Telegram/config.ts
@@ -1,18 +1,19 @@
 // import { restoreAuthKeyFromLocalStorage } from './helpers';
-import { APP_HASH, APP_ID, DEFAULT_DC_ID, SERVER } from './constants';
+import { APP_HASH, APP_ID, DEFAULT_DC_ID, SERVER } from './constants'
 
 export const appSettings = {
   invokeWithLayer: 0xda9b0d0d,
-  layer          : 57,
+  layer          : 67,
   initConnection : 0x69796de9,
   api_id         : APP_ID,
   app_version    : '1.0.1',
   lang_code      : 'en',
-};
+}
 
 export const serverConfig = {
   webogram: location.protocol === 'https:',
-};
+  // dev: true,
+}
 
 export const config = {
   // NOTE: if you FORK the project you MUST use your APP ID.
@@ -21,6 +22,6 @@ export const config = {
   id: APP_ID,
   hash: APP_HASH,
   // authKey: restoreAuthKeyFromLocalStorage(),
-};
+}
 
-export { SERVER, APP_ID, APP_HASH, DEFAULT_DC_ID };
+export { SERVER, APP_ID, APP_HASH, DEFAULT_DC_ID }
--- packages/treact/src/app/helpers/Telegram/pool.ts
+++ packages/treact/src/app/helpers/Telegram/pool.ts
@@ -1,29 +1,44 @@
-import * as localforage from 'localforage';
-import BrowserStorage from 'mtproto-storage-browser';
-import MTProto from 'telegram-mtproto';
-import { appSettings, serverConfig } from './config';
+import * as localforage from 'localforage'
+import BrowserStorage from 'mtproto-storage-browser'
+import MTProto from 'telegram-mtproto'
+import { appSettings, serverConfig } from './config'
 
 export const storage = localforage.createInstance({
   driver: localforage.LOCALSTORAGE,
-});
+})
 
 const app = {
   debug: true,
   storage: new BrowserStorage(storage),
-};
+}
 
 const pool = MTProto({
   server: serverConfig,
   api: appSettings,
   app,
-});
+  schema: require('./api-tlschema-57.json'),
+})
+
+//const ATTEMPTS = 5
+//const TIMEOUT = 40e3
+//const timer = () => new Promise<symbol>(rs => setTimeout(rs, TIMEOUT, timeoutToken))
+//const timeoutToken = Symbol('timeout')
 
 // TODO: use generic params and options
-export const api = <T>(method: string, params?: object, options?: object): Promise<T> =>
-  pool<T>(method, params, options);
-
-if (process.env.NODE_ENV === 'development') {
-  window.api = api;
+export async function api<T>(method: string, params?: object, options?: object): Promise<T> {
+  //let count = 0
+  //do {
+    //const result = await Promise.race([
+      //timer(),
+      return pool<T>(method, params, options)//,
+    //])
+    //if (typeof result !== 'symbol') return result
+  //} while (count++ > ATTEMPTS)
+  //throw new Error(`Api timeout. ${method}`)
 }
 
-export default pool;
+if (process.env.NODE_ENV === 'development') {
+  window.api = api
+}
+
+export default pool
