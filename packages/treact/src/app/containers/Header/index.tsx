c7e8ca3d07c3f57f4c389bc1475a0cbdea9c9184
andretshurotshka <andwebar@gmail.com>
2018-07-22 04:33:54 +0500

    refactor(HeaderContainer): update store paths

--- packages/treact/src/app/containers/Header/index.tsx
+++ packages/treact/src/app/containers/Header/index.tsx
@@ -1,22 +1,18 @@
 import { logOut } from 'api/auth'
 import { switchNightMode } from 'api/theme'
-import { Header } from 'components'
+import { Header } from 'components/Header'
 import { connect } from 'react-redux'
-import { Dispatch } from 'redux/store.h'
+import { Dispatch } from 'store/store.h'
 
 interface ConnectedActions {
   logOut(): void
   switchNightMode(): void
 }
 
-const dispatchToProps = (dispatch: Dispatch) => ({
-  logOut() {
-    dispatch(logOut())
-  },
-  switchNightMode() {
-    dispatch(switchNightMode())
-  },
-})
+const dispatchToProps = {
+  logOut,
+  switchNightMode,
+}
 
 const connected = connect<{}, ConnectedActions, {}>(null, dispatchToProps)(Header)
 
