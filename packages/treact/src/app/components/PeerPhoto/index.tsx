c1cc12a41f9c4855b6e43ba2c1bddfebc892f719
andretshurotshka <andwebar@gmail.com>
2018-07-22 04:31:52 +0500

    refactor(PeerPhoto): make optional className

--- packages/treact/src/app/components/PeerPhoto/index.tsx
+++ packages/treact/src/app/components/PeerPhoto/index.tsx
@@ -4,7 +4,7 @@ import * as defPhoto from './usercolor1.png'
 
 interface Props {
   id: number | 'default',
-  className: string
+  className?: string
 }
 
 export const PeerPhotoEmpty = ({ className }: Pick<Props, 'className'>) =>
