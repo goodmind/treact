4946a36dfe75ada9ab786c9f85932a6a87927b79
andretshurotshka <andwebar@gmail.com>
2018-07-22 04:26:38 +0500

    refactor(client): add spinner

--- packages/treact/src/client.tsx
+++ packages/treact/src/client.tsx
@@ -5,6 +5,7 @@ import * as ReactDOM from 'react-dom'
 import { AppContainer } from 'react-hot-loader'
 import { Provider } from 'react-redux'
 import { persistStore } from 'redux-persist'
+import { CircularLoading } from 'respinner'
 import routes, { resolveRoute } from 'routes'
 import { configureStore } from 'store/store'
 import UserSelectedTheme from 'themes/UserSelectedTheme'
@@ -18,38 +19,52 @@ let currentRoutes = routes
 export const store = configureStore()
 
 const renderComponent = (Component: JSX.Element | null) => {
-  return ReactDOM.render((
+  return ReactDOM.render(
     <AppContainer>
       <Provider store={store} key="provider">
-        <UserSelectedTheme>
-          {Component}
-        </UserSelectedTheme>
+        <UserSelectedTheme>{Component}</UserSelectedTheme>
       </Provider>
-    </AppContainer>
-  ), document.getElementById('app'))
+    </AppContainer>,
+    document.getElementById('app'),
+  )
 }
 
 const onPersist = () => render(history.location, currentRoutes)
 const onHistory = (location: Location) => render(location, currentRoutes)
 const init = () => {
   history.listen(onHistory)
-  renderComponent(<div>Loading...</div>)
-  persistStore(store,
-    {whitelist: [
-      'authKey',
-      'currentUser',
-      'currentDc',
-      'selected',
-    ]}, onPersist)
+  renderComponent(
+    <div
+      style={{
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+        height: '100%',
+      }}
+    >
+      <CircularLoading size={40} duration={1} stroke="#4197ff" />
+    </div>,
+  )
+  persistStore(
+    store,
+    {
+      whitelist: ['authKey', 'currentUser', 'currentDc', 'selected']
+    },
+    onPersist,
+  )
 
   console.log(history.location)
 }
 
 const render = async (location: Location, Routes: typeof routes) => {
-  if (routes)
-    currentRoutes = Routes
-  const Router = new UniversalRouter(currentRoutes, { resolveRoute, context: { store } })
-  const Route = await Router.resolve<JSX.Element>({ pathname: location.pathname })
+  if (routes) currentRoutes = Routes
+  const Router = new UniversalRouter(currentRoutes, {
+    resolveRoute,
+    context: { store },
+  })
+  const Route = await Router.resolve<JSX.Element>({
+    pathname: location.pathname,
+  })
   return renderComponent(<App>{Route}</App>)
 }
 
