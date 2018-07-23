b1dfc749204f3ec9b367b5772b5997b3af4ba2fd
egoarka <egoarka@gmail.com>
2018-07-24 00:53:09 +0300

    refactor(ChatListItem): hoist shortPreview from component

--- packages/treact/src/app/containers/ChatListItem/index.tsx
+++ packages/treact/src/app/containers/ChatListItem/index.tsx
@@ -1,55 +1,78 @@
 import * as React from 'react'
 
-import { isEmpty, path } from 'ramda'
+import { isEmpty, path, propIs, cond, prop, T } from 'ramda'
 
 import { ChatListItem, ChatListItemEmpty } from 'components/ChatListItem'
-import { getPeerData, getPeerName, getPeerShortName } from 'helpers/Telegram/Peers'
+import {
+  getPeerData,
+  getPeerName,
+  getPeerShortName,
+} from 'helpers/Telegram/Peers'
 import { connect } from 'react-redux'
 import { selectChat } from 'store/api/chatList'
-import { StoreHistory } from 'store/modules/histories'
+import { StoreHistories } from 'store/modules/histories'
 import { TPeersType } from 'store/modules/peers'
-import { MtpChat, MtpDialog, MtpMessage /* MtpMessage */, MtpUser } from 'store/mtproto'
+import {
+  MtpChat,
+  MtpDialog,
+  MtpMessage /* MtpMessage */,
+  MtpUser,
+} from 'store/mtproto'
 import { Dispatch, Store } from 'store/store.h'
 
+import { PreviewMedia } from 'containers/Media'
+
 // const getLastMessage =
-//  (history: StoreHistory): MtpMessage => history.byId[last<number, typeof history.ids>(history.ids)];
+//  (history: StoreHistories): MtpMessage => history.byId[last<number, typeof history.ids>(history.ids)];
 
 class ChatListItemContainer extends React.Component<Props & Funcs & State, {}> {
   public renderEmptyItem = () => {
     const { id, selected, peer, peerData } = this.props
-    return <ChatListItemEmpty
-      id={id}
-      click={this.click}
-      selected={selected}
-      name={getPeerName(peer, peerData)}
+    return (
+      <ChatListItemEmpty
+        id={id}
+        click={this.click}
+        selected={selected}
+        name={getPeerName(peer, peerData)}
       />
+    )
   }
   public renderItem = () => {
-    const { id, selected, peer, peerData, from, fromData, isNotChat, isYou, message } = this.props
-    return <ChatListItem
-      id={id}
-      click={this.click}
-      selected={selected}
-      name={getPeerName(peer, peerData)}
-      unreadCount={this.props.dialog.unread_count}
-      previewName={isNotChat ? '' : (getPeerShortName(from, fromData) || '')}
-      isYou={isYou}
-      message={message}
+    const {
+      id,
+      selected,
+      peer,
+      peerData,
+      from,
+      fromData,
+      isNotChat,
+      isYou,
+      message,
+    } = this.props
+    return (
+      <ChatListItem
+        id={id}
+        click={this.click}
+        selected={selected}
+        name={getPeerName(peer, peerData)}
+        unreadCount={this.props.dialog.unread_count}
+        previewName={isNotChat ? '' : getPeerShortName(from, fromData) || ''}
+        isYou={isYou}
+        message={message}
       />
+    )
   }
   public click = () => this.props.click(this.props.id)
   public render() {
     const { history } = this.props
-    return isEmpty(history)
-      ? this.renderEmptyItem()
-      : this.renderItem()
+    return isEmpty(history) ? this.renderEmptyItem() : this.renderItem()
   }
 }
 
 interface Props {
   id: number
   dialog: MtpDialog
-  history: StoreHistory
+  history: StoreHistories
   peer: TPeersType
   selected: boolean
 }
@@ -66,6 +89,7 @@ interface State {
   isNotChat: boolean
   isYou: boolean
   message: MtpMessage
+  shortPreview: React.ReactNode | string
 }
 
 const preview = (state: Store, message: MtpMessage, peer: TPeersType) => {
@@ -84,15 +108,26 @@ const preview = (state: Store, message: MtpMessage, peer: TPeersType) => {
   }
 }
 
+const messageIsNotEmpty = (data: MtpMessage) => !isEmpty(data.message)
+
+const shortPreview = cond<MtpMessage, React.ReactNode>([
+  [messageIsNotEmpty, prop('message')],
+  [propIs(Object, 'media'), ({ media }) => <PreviewMedia media={media} />],
+  [T, () => 'Unknown case'],
+])
+
 const messagesPath = path(['messages', 'byId'])
 
 const mapState = (state: Store, { id, peer, dialog }: Props): State => {
-  const message = (messagesPath(state) as Store['messages']['byId'])[dialog.top_message]
+  const message = (messagesPath(state) as Store['messages']['byId'])[
+    dialog.top_message
+  ]
   const peerData = getPeerData(id, peer, state)
 
   return {
     peerData,
     message,
+    shortPreview: shortPreview(message),
     ...preview(state, message, peer),
   }
 }
@@ -101,6 +136,10 @@ const mapDispatch = (dispatch: Dispatch) => ({
   click: (id: number) => dispatch(selectChat(id)),
 })
 
-const connected = connect<State, Funcs, Props, Store>(mapState, mapDispatch)(ChatListItemContainer)
+const connected = connect<State, Funcs, Props, Store>(
+  mapState,
+  mapDispatch,
+)(ChatListItemContainer)
 
+export { shortPreview }
 export { connected as ChatListItem }
